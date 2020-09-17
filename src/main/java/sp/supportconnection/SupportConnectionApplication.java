45fc36f5be80515c6230254fe3c694d367931814
kimsutto <themerciless2016@gmail.com>
2020-09-17 17:41:35 +0900

    initial spring boot project

---
+++ src/main/java/sp/supportconnection/SupportConnectionApplication.java
@@ -0,0 +1,13 @@
+package sp.supportconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sp/supportconnection/SupportConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.supportconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
