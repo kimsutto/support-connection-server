d3bf3274ecf63488c38b34f2b5d78dfa8f57d877
kimsutto <themerciless2016@gmail.com>
2020-09-19 03:55:15 +0900

    유저 정보 입력 시 지원 금액 업데이트 기능 추가

--- src/main/java/sp/supportconnection/service/ConditionService.java
+++ src/main/java/sp/supportconnection/service/ConditionService.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sp.supportconnection.dto.ConditionRequest;
+import sp.supportconnection.entity.AvailableSupport;
 import sp.supportconnection.entity.Condition;
 import sp.supportconnection.entity.Support;
 import sp.supportconnection.entity.User;
+import sp.supportconnection.repository.AvailableSupportRepository;
 import sp.supportconnection.repository.ConditionRepository;
 import sp.supportconnection.repository.SupportRepository;
 import sp.supportconnection.repository.UserRepository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -21,12 +24,14 @@ public class ConditionService {
     private final ConditionRepository conditionRepository;
     private final SupportRepository supportRepository;
     private final UserRepository userRepository;
+    private final AvailableSupportRepository availableSupportRepository;
 
     @Transactional
     public void updateCondition(ConditionRequest request) {
         Long id = request.getUserId();
         User user = userRepository.findById(id).get();
         Long conditionId = user.getCondition().getId();
+        Long availableId = user.getAvailableSupport().getId();
 
         Optional<Condition> condition = conditionRepository.findById(conditionId);
 
@@ -58,13 +63,41 @@ public class ConditionService {
 
         //연관 관계 설정 update하기 -> user-support table도 같이 업데이트
         List<Support> supports = supportRepository.findAll();
-        supports.stream().filter(support -> support.getCondition().equals(condition));
+        supports.stream().filter(support -> matching(support.getCondition(),condition.get()));
+
+
+        //나중에 .. 함수 분리 하겠슴다..
+
+        int totalAmount = 0;
+        int cashAmount = 0;
+        int financialAmount = 0;
+
         for(Support support : supports){
             user.getSupports().add(support);
+            totalAmount += support.getAmount();
+            if(support.getType().equals("현금")){
+                cashAmount += support.getAmount();
+            }else{
+                financialAmount += support.getAmount();
+            }
         }
+        Optional<AvailableSupport> availableSupport = availableSupportRepository.findById(availableId);
+        availableSupport.get().setTotalAmount(totalAmount);
+        availableSupport.get().setCashAmount(cashAmount);
+        availableSupport.get().setFinancialAmount(financialAmount);
 
+        user.setAvailableSupport(availableSupport.get());
+
+        availableSupportRepository.save(availableSupport.get());
         userRepository.save(user);
+
         return;
     }
+
+    boolean matching(Condition a, Condition b){
+        return true;
+    }
+
+
 
 }
