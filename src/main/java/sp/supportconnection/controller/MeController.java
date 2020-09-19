4abc707e81906d64bb65df8d3a32851b9df001c4
jiwon-yang <jiwonyang.tech@gmail.com>
2020-09-19 19:38:03 +0900

    나의 대출정보 입력하기 API 생성

--- src/main/java/sp/supportconnection/controller/MeController.java
+++ src/main/java/sp/supportconnection/controller/MeController.java
@@ -20,7 +20,7 @@ public class MeController {
 
     // 나의 자산 입력하기
     @PostMapping(value = "/me/asset", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity registerMyAsset(@RequestBody MeController.MeRequest request){
+    public ResponseEntity registerMyAsset(@RequestBody MeController.RegisterMyAssetRequest request){
         Asset asset = userService.getAsset(request.getUserId());
         int newMyAsset = request.getMyAsset();
         int newAnnualIncome = request.getAnnualIncome();
@@ -28,12 +28,27 @@ public class MeController {
         return ResponseEntity.ok(newAsset);
     }
     @Data
-    static class MeRequest{
+    static class RegisterMyAssetRequest{
         private Long userId;
         private int myAsset;
         private int annualIncome;
     }
 
     // 나의 대출 정보 입력하기
+    @PostMapping(value = "/me/loan", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity registerMyLoan(@RequestBody MeController.RegisterMyLoanRequest request){
+        Asset asset = userService.getAsset(request.getUserId());
+        int newLoan = request.getLoan();
+        int newInterestRate = request.getInterestRate();
+        Asset newAsset = meService.updateMyLoan(asset, newLoan, newInterestRate);
+        return ResponseEntity.ok(newAsset);
+    }
+    @Data
+    static class RegisterMyLoanRequest{
+        private Long userId;
+        private int loan;
+        private int interestRate;
+    }
+
     // 나의 지원금 잔액 입력하기
 }
